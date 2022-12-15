632558ac85171a11e101e90a76b4483a0bd17f04
Alexander Sulim <hello@sul.im>
2022-12-15 10:04:34 +0100

    Add a skeleton of the main module

--- host/cmd/q/main.go
+++ host/cmd/q/main.go
@@ -10,50 +10,36 @@ import (
 )
 
 func main() {
-	// conf := &host.Config{}
-	// if err := loadConfig(conf); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "err=%v\n", err)
-	// }
-	//
-	// mux := &host.RPCMux{}
-	//
-	// if err := mux.Register("ListCommands", &host.ListCommandsHandler{Commands: conf.Commands}); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "err=%v\n", err)
-	// }
-	//
-	// srv := &host.Server{
-	// 	Handler: mux,
-	// }
-	//
-	// if err := srv.ServeConn(host.NewConn(os.Stdin, os.Stdout)); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "err=%v\n", err)
-	// }
-
 	// Load configuration.
+	// A configuration file is expected to be in $HOME/.config/q/config.toml.
+	conf := &host.Config{}
+
 	u, err := user.Current()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "err=%v\n", err)
 	}
 
-	conf := &host.Config{}
 	if err := conf.Load(filepath.Join(u.HomeDir, ".config", "q", "config.toml")); err != nil {
 		fmt.Fprintf(os.Stderr, "err=%v\n", err)
 	}
 
-	listCommandsProc := &host.ListCommandsProc{
-		Commands: conf.Commands,
-	}
-
+	// Set up a multiplexer to serve supported RPC methods.
 	mux := &host.ServeMux{}
 
-	if err := mux.Register("ListCommands", listCommandsProc);
-}
+	if err := mux.Register("ListCommands", host.NewListCommandsProc(conf.Commands)); err != nil {
+		fmt.Fprintf(os.Stderr, "err=%v\n", err)
+	}
 
-// func loadConfig(conf *host.Config) error {
-// 	u, err := user.Current()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	return conf.Load(filepath.Join(u.HomeDir, ".config", "q", "config.toml"))
-// }
+	if err := mux.Register("RunCommand", host.NewRunCommandProc(conf.Commands)); err != nil {
+		fmt.Fprintf(os.Stderr, "err=%v\n", err)
+	}
+
+	// Set up a server and start serving incoming connections.
+	srv := &host.Server{
+		Handler: mux,
+	}
+
+	if err := srv.ServeConn(host.NewConn(os.Stdin, os.Stdout)); err != nil {
+		fmt.Fprintf(os.Stderr, "err=%v\n", err)
+	}
+}
