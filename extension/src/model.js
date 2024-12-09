1e194dff02c8142cb8a1d1919bcce4aee32fbf62
Alexander Sulim <hello@sul.im>
2024-12-09 20:39:42 +0100

    [WIP] Add tests to extension

--- extension/src/model.js
+++ extension/src/model.js
@@ -1,72 +1,72 @@
 class Model {
-  #connection;
+	#connection;
 
-  #rpcCallbacksQueue;
-  #context;
+	#rpcCallbacksQueue;
+	#context;
 
-  constructor() {
-    this.#rpcCallbacksQueue = new Map();
-    this.#connection = browser.runtime.connect({
-      name: "urn:browser-ext:dev.sulim.q:popup",
-    });
-    this.#connection.onMessage.addListener(this.#onMessage.bind(this));
+	constructor() {
+		this.#rpcCallbacksQueue = new Map();
+		this.#connection = browser.runtime.connect({
+			name: "urn:browser-ext:dev.sulim.q:popup",
+		});
+		this.#connection.onMessage.addListener(this.#onMessage.bind(this));
 
-    // Fetch page context (URL and HTML).
-    const executing = browser.tabs.executeScript({
-      file: "content.js",
-    });
-    executing.then(
-      function (result) {
-        this.#context = result[0];
-      }.bind(this),
-      function () {
-        console.error(arguments);
-      },
-    );
-  }
+		// Fetch page context (URL and HTML).
+		const executing = browser.tabs.executeScript({
+			file: "content.js",
+		});
+		executing.then(
+			function (result) {
+				this.#context = result[0];
+			}.bind(this),
+			() => {
+				console.error(arguments);
+			},
+		);
+	}
 
-  listCommands(callback) {
-    const rpcRequestID = this.#generateRequestID();
-    const rpcRequest = {
-      jsonrpc: "2.0",
-      method: "ListCommands",
-      id: rpcRequestID,
-    };
+	listCommands(callback) {
+		const rpcRequestID = this.#generateRequestID();
+		const rpcRequest = {
+			jsonrpc: "2.0",
+			method: "ListCommands",
+			id: rpcRequestID,
+		};
 
-    this.#rpcCallbacksQueue.set(rpcRequestID, callback);
+		this.#rpcCallbacksQueue.set(rpcRequestID, callback);
 
-    this.#connection.postMessage(rpcRequest);
-  }
+		this.#connection.postMessage(rpcRequest);
+	}
 
-  runCommand(commandID, callback) {
-    const rpcRequestID = this.#generateRequestID();
-    const rpcRequest = {
-      jsonrpc: "2.0",
-      method: "RunCommand",
-      params: [commandID, this.#context],
-      id: rpcRequestID,
-    };
+	runCommand(commandID, callback) {
+		const rpcRequestID = this.#generateRequestID();
+		const rpcRequest = {
+			jsonrpc: "2.0",
+			method: "RunCommand",
+			params: [commandID, this.#context],
+			id: rpcRequestID,
+		};
 
-    this.#rpcCallbacksQueue.set(rpcRequestID, callback);
+		this.#rpcCallbacksQueue.set(rpcRequestID, callback);
 
-    this.#connection.postMessage(rpcRequest);
-  }
+		this.#connection.postMessage(rpcRequest);
+	}
 
-  #onMessage(message, connection) {
-    const callback = this.#rpcCallbacksQueue.get(message.id);
+	#onMessage(message, connection) {
+		const callback = this.#rpcCallbacksQueue.get(message.id);
 
-    if (callback === undefined) {
-      return;
-    }
+		if (callback === undefined) {
+			return;
+		}
 
-    callback(message.result);
-  }
+		callback(message.result);
+	}
 
-  #generateRequestID() {
-    return `urn:browser-ext:dev.sulim.q:rpc-request:${
-      this.#rpcCallbacksQueue.size + 1
-    }`;
-  }
+	#generateRequestID() {
+		return `urn:browser-ext:dev.sulim.q:rpc-request:${
+			this.#rpcCallbacksQueue.size + 1
+		}`;
+	}
 }
 
 export { Model };
--- extension/src/rpc_server.js
+++ extension/src/rpc_server.js
@@ -1,93 +1,91 @@
 class RPCServer {
-  #connections;
+	#connections;
 
-  constructor() {
-    this.#connections = [];
-  }
+	constructor() {
+		this.#connections = [];
+	}
 
-  start() {
-    browser.runtime.onConnect.addListener(this.#onConnect.bind(this));
-  }
+	start() {
+		browser.runtime.onConnect.addListener(this.#onConnect.bind(this));
+	}
 
-  #onConnect(connection) {
-    if (connection.name !== "urn:browser-ext:dev.sulim.q:popup") {
-      return;
-    }
+	#onConnect(connection) {
+		if (connection.name !== "urn:browser-ext:dev.sulim.q:popup") {
+			return;
+		}
 
-    connection.onMessage.addListener(this.#onMessage.bind(this));
+		connection.onMessage.addListener(this.#onMessage.bind(this));
 
-    this.#connections.push(connection);
-  }
+		this.#connections.push(connection);
+	}
 
-  #onMessage(message, connection) {
-    const rpcRequest = {
-      jsonrpc: "2.0",
-      id: message.id,
-    };
-    const onError = function (error) {
-      console.debug("RPCServer#onMessage/onError");
-      console.debug(error);
-    };
+	#onMessage(message, connection) {
+		const rpcRequest = {
+			jsonrpc: "2.0",
+			id: message.id,
+		};
+		const onError = (error) => {
+			console.debug("RPCServer#onMessage/onError");
+			console.debug(error);
+		};
 
-    switch (message.method) {
-      case "ListCommands": {
-        const onSuccess = function (response) {
-          connection.postMessage({
-            jsonrpc: "2.0",
-            result: response.result,
-            id: response.id,
-          });
-        };
+		switch (message.method) {
+			case "ListCommands": {
+				const onSuccess = (response) => {
+					connection.postMessage({
+						jsonrpc: "2.0",
+						result: response.result,
+						id: response.id,
+					});
+				};
 
-        rpcRequest.method = "ListCommands";
+				rpcRequest.method = "ListCommands";
 
-        this.#sendNativeMessage(rpcRequest, onSuccess, onError);
-        break;
-      }
-      case "RunCommand": {
-        const commandID = message.params[0];
-        const pageURL =
-          message.params[1] !== undefined ? message.params[1].url : null;
-        const pageHTML =
-          message.params[1] !== undefined ? message.params[1].html : null;
+				this.#sendNativeMessage(rpcRequest, onSuccess, onError);
+				break;
+			}
+			case "RunCommand": {
+				const commandID = message.params[0];
+				const pageURL =
+					message.params[1] !== undefined ? message.params[1].url : null;
+				const pageHTML =
+					message.params[1] !== undefined ? message.params[1].html : null;
 
-        rpcRequest.method = "RunCommand";
-        rpcRequest.params = [commandID, pageURL, pageHTML];
+				rpcRequest.method = "RunCommand";
+				rpcRequest.params = [commandID, pageURL, pageHTML];
 
-        const onSuccess = function (response) {
-          console.debug("RPCServer#onMessage/onSuccess");
-          console.debug(response);
+				const onSuccess = (response) => {
+					console.debug("RPCServer#onMessage/onSuccess");
+					console.debug(response);
 
-          if (response.result !== "") {
-            browser.notifications.create({
-              "type": "basic",
-              "title": "Response from Q command",
-              "message": response.result
-            });
-          }
-        };
+					if (response.result !== "") {
+						browser.notifications.create({
+							type: "basic",
+							title: "Response from Q command",
+							message: response.result,
+						});
+					}
+				};
 
-        this.#sendNativeMessage(rpcRequest, onSuccess, onError);
+				this.#sendNativeMessage(rpcRequest, onSuccess, onError);
 
-        connection.postMessage({
-          jsonrpc: "2.0",
-          result: null,
-          id: message.id,
-        });
-        break;
-      }
-      default: {
-        console.debug(message);
-      }
-    }
-  }
+				connection.postMessage({
+					jsonrpc: "2.0",
+					result: null,
+					id: message.id,
+				});
+				break;
+			}
+			default: {
+				console.debug(message);
+			}
+		}
+	}
 
-  #sendNativeMessage(message, onSuccess, onError) {
-    const sending = browser.runtime.sendNativeMessage("dev.sulim.q", message);
-    sending.then(onSuccess.bind(this), onError.bind(this));
-  }
+	#sendNativeMessage(message, onSuccess, onError) {
+		const sending = browser.runtime.sendNativeMessage("dev.sulim.q", message);
+		sending.then(onSuccess.bind(this), onError.bind(this));
+	}
 }
 
-export {
-  RPCServer,
-}
+export { RPCServer };
